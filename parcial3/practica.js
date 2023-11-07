78ce77984236d125cfb4c06472b1e4990fc99f64
karlamtzhdz <132524476+karlamtzhdz@users.noreply.github.com>
2023-11-06 21:37:04 -0600

    Add files via upload
    calculadora

---
+++ parcial3/practica.js
@@ -0,0 +1,31 @@
+
+
+function suma(){
+    let n1, n2, calificacion
+    n1 =prompt("numero1")
+    n2 =prompt("numero2")
+    suma = parseInt(n1) + parseInt(n2)
+    alert(suma)
+}
+function resta(){
+    let n1, n2, calificacion
+    n1 =prompt("numero1")
+    n2 =prompt("numero2")
+    resta = parseInt(n1) - parseInt(n2)
+    alert(resta)
+}
+function multiplicacion(){
+    let n1, n2, calificacion
+    n1 =prompt("numero1")
+    n2 =prompt("numero2")
+    multiplicacion = parseInt(n1) * parseInt(n2)
+    alert(multiplicacion)
+}
+function division(){
+    let n1, n2, calificacion
+    n1 =prompt("numero1")
+    n2 =prompt("numero2")
+    division = parseInt(n1) / parseInt(n2)
+    alert(division)
+}
+
